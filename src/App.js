35bd507b9d898c0f91dc686f83b27a02e1e40d84
gtanchak <ghtanchak77@gmail.com>
2026-01-08 14:21:54 +0530

    working on the core react implementation

---
+++ src/App.js
@@ -0,0 +1,26 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Parde Gino's"),
+    React.createElement(Pizza, {
+      name: "7 cheesy",
+      description: "7 cube with Majarrela Cheese",
+    }),
+    React.createElement(Pizza, {
+      name: "Farm House",
+      description: "Vegetable toping Souces.",
+    })
+  );
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
